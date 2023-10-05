92e42de1a0ad9edba9dc4e35cec8024c41ecf31c
Allan <allan@4gl.io>
2023-10-05 18:34:08 +0100

    feat: actual mock

--- BE/sasjs/mocks/services/common/appinit.js
+++ BE/sasjs/mocks/services/common/appinit.js
@@ -1,21 +1,31 @@
-_webout = {
-  SYSDATE: '26SEP22',
-  SYSTIME: '08:00',
-  areas: [
-    { AREA: 'Adak' },
-    { AREA: 'Adel' },
-    { AREA: 'Afognak' },
-    { AREA: 'Ajo' },
-    { AREA: 'Albany' },
-    { AREA: 'Albuquerque' },
-    { AREA: 'Alturas' },
-    { AREA: 'Ashton' },
-    { AREA: 'Atka' },
-    { AREA: 'Atlanta' },
-    { AREA: 'Attu' },
-    { AREA: 'Aztec' }
+_webout = `{
+  "SYSDATE": "05OCT23",
+  "SYSTIME": "14:33",
+  "groups": [
+    {
+      "ROLE": "BEADM",
+      "NOMBRE": "s14325174"
+    }
   ],
-  MF_GETUSER: 'Chad'
-}
-
-_webout = JSON.stringify(_webout)
+  "_DEBUG": "",
+  "_METAUSER": "ricardo.quezada",
+  "_METAPERSON": "s14325174",
+  "_PROGRAM": "/Boleta/Migracion/sas9/services/common/appinit",
+  "AUTOEXEC": "%2Fsasbin%2FSASConfig%2FLev1%2FSASAppEGRC%2FWorkspaceServer%2Fautoexec.sas",
+  "MF_GETUSER": "s14325174",
+  "SYSCC": "0",
+  "SYSENCODING": "utf-8",
+  "SYSERRORTEXT": "",
+  "SYSHOSTNAME": "sascsdes301",
+  "SYSPROCESSID": "41DDFBB02B4EBF2B4034000000000000",
+  "SYSPROCESSMODE": "SAS Workspace Server",
+  "SYSPROCESSNAME": "",
+  "SYSJOBID": "47743",
+  "SYSSCPL": "Linux",
+  "SYSSITE": "70205261",
+  "SYSUSERID": "ricardo.quezada",
+  "SYSVLONG": "9.04.01M7P080620",
+  "SYSWARNINGTEXT": "",
+  "END_DTTM": "2023-10-05T14:33:20.161978",
+  "MEMSIZE": "10GB"
+}`
